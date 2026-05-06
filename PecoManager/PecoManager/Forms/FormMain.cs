27aa3c0b2e400d29935960ce26807120c1705529
dorogoi Iulian <emailul_tu_de_pe_GitHub>
2026-05-06 12:50:32 +0300

    feat: adaugă FormMain cu MenuStrip și navigare (#12)

---
+++ PecoManager/PecoManager/Forms/FormMain.cs
@@ -0,0 +1,39 @@
+﻿namespace PecoManager.Forms
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stațiiPECOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormStatii form = new FormStatii();
+            form.Show();
+        }
+
+        private void combustibiliToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormCombustibil form = new FormCombustibil();
+            form.Show();
+        }
+
+        private void tranzacțiiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormTranzactii form = new FormTranzactii();
+            form.Show();
+        }
+
+        private void rapoarteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormRaport form = new FormRaport();
+            form.Show();
+        }
+    }
+}
