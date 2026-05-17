7b687408794cb531a95f0e71eea9fffe766890ae
dorogoi Iulian <emailul_tu_de_pe_GitHub>
2026-05-17 20:50:21 +0300

    feat: implementează FormStatii cu DataGridView și butoane CRUD (#17)

--- PecoManager/PecoManager/Forms/FormStatii.Designer.cs
+++ PecoManager/PecoManager/Forms/FormStatii.Designer.cs
@@ -2,38 +2,114 @@
 {
     partial class FormStatii
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormStatii";
+            lblTitlu = new Label();
+            txtCautare = new TextBox();
+            btnCauta = new Button();
+            btnAdauga = new Button();
+            btnModifica = new Button();
+            btnSterge = new Button();
+            dataGridView = new DataGridView();
+
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+
+            // lblTitlu
+            lblTitlu.AutoSize = true;
+            lblTitlu.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            lblTitlu.Location = new Point(12, 12);
+            lblTitlu.Text = "Gestionare Stații PECO";
+
+            // txtCautare
+            txtCautare.Location = new Point(12, 55);
+            txtCautare.PlaceholderText = "Caută după denumire sau oraș...";
+            txtCautare.Size = new Size(350, 23);
+
+            // btnCauta
+            btnCauta.Location = new Point(370, 54);
+            btnCauta.Size = new Size(80, 25);
+            btnCauta.Text = "Caută";
+            btnCauta.BackColor = Color.FromArgb(70, 130, 180);
+            btnCauta.ForeColor = Color.White;
+            btnCauta.FlatStyle = FlatStyle.Flat;
+            btnCauta.Click += btnCauta_Click;
+
+            // btnAdauga
+            btnAdauga.Location = new Point(460, 54);
+            btnAdauga.Size = new Size(90, 25);
+            btnAdauga.Text = "+ Adaugă";
+            btnAdauga.BackColor = Color.FromArgb(60, 179, 113);
+            btnAdauga.ForeColor = Color.White;
+            btnAdauga.FlatStyle = FlatStyle.Flat;
+            btnAdauga.Click += btnAdauga_Click;
+
+            // btnModifica
+            btnModifica.Location = new Point(560, 54);
+            btnModifica.Size = new Size(90, 25);
+            btnModifica.Text = "✏ Modifică";
+            btnModifica.BackColor = Color.FromArgb(255, 165, 0);
+            btnModifica.ForeColor = Color.White;
+            btnModifica.FlatStyle = FlatStyle.Flat;
+            btnModifica.Click += btnModifica_Click;
+
+            // btnSterge
+            btnSterge.Location = new Point(660, 54);
+            btnSterge.Size = new Size(80, 25);
+            btnSterge.Text = "🗑 Șterge";
+            btnSterge.BackColor = Color.FromArgb(220, 53, 69);
+            btnSterge.ForeColor = Color.White;
+            btnSterge.FlatStyle = FlatStyle.Flat;
+            btnSterge.Click += btnSterge_Click;
+
+            // dataGridView
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(12, 90);
+            dataGridView.MultiSelect = false;
+            dataGridView.Name = "dataGridView";
+            dataGridView.ReadOnly = true;
+            dataGridView.RowHeadersVisible = false;
+            dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView.Size = new Size(760, 420);
+            dataGridView.TabIndex = 0;
+
+            // FormStatii
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 531);
+            Controls.Add(lblTitlu);
+            Controls.Add(txtCautare);
+            Controls.Add(btnCauta);
+            Controls.Add(btnAdauga);
+            Controls.Add(btnModifica);
+            Controls.Add(btnSterge);
+            Controls.Add(dataGridView);
+            Name = "FormStatii";
+            Text = "Gestionare Stații PECO";
+            Load += FormStatii_Load;
+
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
-        #endregion
+        private Label lblTitlu;
+        private TextBox txtCautare;
+        private Button btnCauta;
+        private Button btnAdauga;
+        private Button btnModifica;
+        private Button btnSterge;
+        private DataGridView dataGridView;
     }
 }
--- PecoManager/PecoManager/Forms/FormStatii.cs
+++ PecoManager/PecoManager/Forms/FormStatii.cs
@@ -1,11 +1,91 @@
-﻿namespace PecoManager.Forms
+﻿using PecoManager.Data;
+using PecoManager.Models;
+
+namespace PecoManager.Forms
 {
     public partial class FormStatii : Form
     {
         public FormStatii()
         {
             InitializeComponent();
-            this.Text = "Gestionare Stații PECO";
+        }
+
+        private void FormStatii_Load(object sender, EventArgs e)
+        {
+            IncarcaStatii();
+        }
+
+        private void IncarcaStatii()
+        {
+            var statii = DatabaseHelper.GetAllStatii();
+            dataGridView.DataSource = statii;
+
+            if (dataGridView.Columns.Count > 0)
+            {
+                dataGridView.Columns["IdStatie"].Visible = false;
+                dataGridView.Columns["Denumire"].HeaderText = "Denumire";
+                dataGridView.Columns["Adresa"].HeaderText = "Adresă";
+                dataGridView.Columns["Oras"].HeaderText = "Oraș";
+                dataGridView.Columns["NrPompe"].HeaderText = "Nr. Pompe";
+                dataGridView.Columns["Status"].HeaderText = "Status";
+            }
+        }
+
+        private void btnCauta_Click(object sender, EventArgs e)
+        {
+            string termen = txtCautare.Text.Trim();
+            if (string.IsNullOrEmpty(termen))
+            {
+                IncarcaStatii();
+                return;
+            }
+            var rezultate = DatabaseHelper.SearchStatii(termen);
+            dataGridView.DataSource = rezultate;
+        }
+
+        private void btnAdauga_Click(object sender, EventArgs e)
+        {
+            FormStatieEdit form = new FormStatieEdit();
+            if (form.ShowDialog() == DialogResult.OK)
+                IncarcaStatii();
+        }
+
+        private void btnModifica_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.CurrentRow == null)
+            {
+                MessageBox.Show("Selectați o stație pentru modificare!",
+                    "Avertizare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var statie = (StatiePeco)dataGridView.CurrentRow.DataBoundItem;
+            FormStatieEdit form = new FormStatieEdit(statie);
+            if (form.ShowDialog() == DialogResult.OK)
+                IncarcaStatii();
+        }
+
+        private void btnSterge_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.CurrentRow == null)
+            {
+                MessageBox.Show("Selectați o stație pentru ștergere!",
+                    "Avertizare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var statie = (StatiePeco)dataGridView.CurrentRow.DataBoundItem;
+            var confirmare = MessageBox.Show(
+                $"Sigur doriți să ștergeți stația '{statie.Denumire}'?",
+                "Confirmare ștergere",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (confirmare == DialogResult.Yes)
+            {
+                DatabaseHelper.DeleteStatie(statie.IdStatie);
+                IncarcaStatii();
+            }
         }
     }
 }
